0e2674e22ef2d17aaf6c478c53aa0b6930ab91cd
Inam Ullah <youinamullah@gmail.com>
2023-10-14 22:23:50 +0500

    initial commit

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+import { getMyProfile, login, logout, register } from "../controllers/user.js";
+
+// const express = require(express);
+import express from "express";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post("/new", register);
+router.post("/login", login);
+router.get("/logout", logout);
+router.get("/me", isAuthenticated, getMyProfile);
+
+export default router;
---
+++ utils/features.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const sendCookie = (user, res, message, statusCode = 200) => {
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+  // Set the token as an HTTP-only cookie
+  res.status(statusCode).cookie("token", token, {
+    httpOnly: true,
+    maxAge: 15 * 60 * 1000, // 15 minutes in milliseconds
+    sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+    secure: process.env.NODE_ENV === "Development" ? false : true,
+  });
+
+  return res.status(201).json({
+    success: true,
+    message,
+  });
+};
