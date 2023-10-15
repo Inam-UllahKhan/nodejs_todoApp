1f1e06126519855f730e6130a7581b79c558908d
Inam Ullah <youinamullah@gmail.com>
2023-10-16 02:00:04 +0500

    go back from 2nd change

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-// import express from "express";
-
-
-
-
-
-
-
-
-
 import express from "express";
 import userRouter from "./routes/user.js";
 import taskRouter from "./routes/task.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
+
 import cors from "cors";
 
 export const app = express();
@@ -25,9 +16,13 @@ config({
 // Using middlewares
 app.use(express.json());
 app.use(cookieParser());
-
-// Enable CORS for all routes
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Using routes
 app.use("/api/v1/users", userRouter);
